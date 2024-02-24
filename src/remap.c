72e98dd7b54fe351d4c77cb99f03244bb5702f70
benbaker76 <headkaze@gmail.com>
2024-02-24 12:33:04 -0800

    Write png only and maintain palette order

--- src/remap.c
+++ src/remap.c
@@ -10,6 +10,7 @@
 #include "convert.h"
 #include "diff.h"
 #include "palette.h"
+#include "lodepng.h"
 
 const char* get_filename_ext(const char* filename) {
     const char* dot = strrchr(filename, '.');
@@ -73,25 +74,28 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "The file extension \"%s\" is not supported. Please use one of the following instead: act, pal, gpl, txt, png\n", paletteFilename);
 	}
 
-    rgbcolor* rgbPaletteColors = NULL;
+    rgbcolor* uniqueRgbPaletteColors = NULL;
+    int countOfUniquePaletteColors = 0;
 
     if (colorPalette != NULL)
     {
         paletteWidth = paletteCount;
         paletteHeight = 1;
         paletteFormat = 3;
+        countOfUniquePaletteColors = paletteCount;
 
-        rgbPaletteColors = (rgbcolor*) malloc(paletteCount * sizeof(rgbcolor));
+        uniqueRgbPaletteColors = (rgbcolor*) malloc(paletteCount * sizeof(rgbcolor));
         for (int i = 0; i < paletteCount; i++) {
             float R = (float) colorPalette[i].R;
             float G = (float) colorPalette[i].G;
             float B = (float) colorPalette[i].B;
-            rgbcolor_init(rgbPaletteColors + i, R, G, B);
+            rgbcolor_init(uniqueRgbPaletteColors + i, R, G, B);
         }
     }
     else
     {
-        rgbPaletteColors = (rgbcolor*) malloc(paletteWidth * paletteHeight * sizeof(rgbcolor));
+        rgbcolor* rgbPaletteColors = (rgbcolor*) malloc(paletteWidth * paletteHeight * sizeof(rgbcolor));
+        uniqueRgbPaletteColors = (rgbcolor*) malloc(paletteWidth * paletteHeight * sizeof(rgbcolor));
         for (int i = 0; i < paletteWidth * paletteHeight; i++) {
             float R = (float) paletteImage[i * paletteFormat + 0];
             float G = (float) paletteImage[i * paletteFormat + 1];
@@ -110,116 +114,105 @@ int main(int argc, char** argv) {
             
             rgbcolor_init(rgbPaletteColors + i, R, G, B);
         }
-    }
 
-    qsort(rgbPaletteColors, paletteWidth * paletteHeight, sizeof(rgbcolor), compareRgbcolor);
+        qsort(rgbPaletteColors, paletteWidth * paletteHeight, sizeof(rgbcolor), compareRgbcolor);
 
-    rgbcolor uniqueRgbPaletteColors[paletteWidth * paletteHeight];
-    int countOfUniquePaletteColors = 0;
-    for (int i = 0; i < paletteWidth * paletteHeight; i++) {
-        rgbcolor* rgbPaletteColor = rgbPaletteColors + i;
-        float R = rgbPaletteColor->R;
-        float G = rgbPaletteColor->G;
-        float B = rgbPaletteColor->B;
-        if (i == 0) {
-            rgbcolor_init(uniqueRgbPaletteColors + countOfUniquePaletteColors++, R, G, B);
-        } else {
-            rgbcolor* lastUniqueRgbPaletteColor = uniqueRgbPaletteColors + countOfUniquePaletteColors - 1;
-            if (R != lastUniqueRgbPaletteColor->R || G != lastUniqueRgbPaletteColor->G || B != lastUniqueRgbPaletteColor->B) {
+        rgbcolor uniqueRgbPaletteColors[paletteWidth * paletteHeight];
+        int countOfUniquePaletteColors = 0;
+        for (int i = 0; i < paletteWidth * paletteHeight; i++) {
+            rgbcolor* rgbPaletteColor = rgbPaletteColors + i;
+            float R = rgbPaletteColor->R;
+            float G = rgbPaletteColor->G;
+            float B = rgbPaletteColor->B;
+            if (i == 0) {
                 rgbcolor_init(uniqueRgbPaletteColors + countOfUniquePaletteColors++, R, G, B);
+            } else {
+                rgbcolor* lastUniqueRgbPaletteColor = uniqueRgbPaletteColors + countOfUniquePaletteColors - 1;
+                if (R != lastUniqueRgbPaletteColor->R || G != lastUniqueRgbPaletteColor->G || B != lastUniqueRgbPaletteColor->B) {
+                    rgbcolor_init(uniqueRgbPaletteColors + countOfUniquePaletteColors++, R, G, B);
+                }
             }
         }
-    }
 
-    free(rgbPaletteColors);
+        free(rgbPaletteColors);
+    }
 
     labcolor uniqueLabPaletteColors[countOfUniquePaletteColors];
     for (int i = 0; i < countOfUniquePaletteColors; i++) {
         rgb_to_lab(uniqueRgbPaletteColors + i, uniqueLabPaletteColors + i);
     }
+    
+    LodePNGState state;
+    lodepng_state_init(&state);
 
-    unsigned char* outputImage = (unsigned char*) malloc(inputWidth * inputHeight * inputFormat * sizeof(unsigned char));
-    if (!outputImage) {
-        perror("Failed to allocate memory for outputImage");
-        return EXIT_FAILURE;
+    for (int i = 0; i < countOfUniquePaletteColors; i++) {
+        lodepng_palette_add(&state.info_png.color, uniqueRgbPaletteColors[i].R, uniqueRgbPaletteColors[i].G, uniqueRgbPaletteColors[i].B, 255);
+        lodepng_palette_add(&state.info_raw, uniqueRgbPaletteColors[i].R, uniqueRgbPaletteColors[i].G, uniqueRgbPaletteColors[i].B, 255);
     }
 
-    rgbcolor** remap = calloc(256 * 256 * 256, sizeof(rgbcolor*));
-    if (!remap) {
-        perror("Failed to allocate memory for remap");
+    state.info_png.color.colortype = LCT_PALETTE;
+    state.info_png.color.bitdepth = 4;
+    state.info_raw.colortype = LCT_PALETTE;
+    state.info_raw.bitdepth = 4;
+    state.encoder.auto_convert = 0;
+
+    int imageSize = (inputWidth * inputHeight * 4 + 7) / 8;
+    unsigned char* image = (unsigned char*)malloc(imageSize);
+    memset(image, 0, imageSize);
+    if (!image) {
+        perror("Failed to allocate memory for image");
         return EXIT_FAILURE;
     }
 
     for (int i = 0; i < inputWidth * inputHeight; i++) {
+        size_t byte_index = i / 2;
+        int byte_half = i % 2 == 1;
         float R = (float) inputImage[i * inputFormat + 0];
         float G = (float) inputImage[i * inputFormat + 1];
         float B = (float) inputImage[i * inputFormat + 2];
 
-        if (inputFormat == 4) {
-            float A = (float) inputImage[i * inputFormat + 3] / 255.0f;
-            if (A == 0.0f) {
-                outputImage[i * inputFormat + 3] = inputImage[i * inputFormat + 3];
-                continue;
-            }
-            rgbacolor rgba;
-            rgbacolor_init(&rgba, R, G, B, A);
-            rgbcolor rgb;
-            rgba_to_rgb(&rgba, &rgb, NULL);
-            R = rgb.R;
-            G = rgb.G;
-            B = rgb.B;
-        }
-
         rgbcolor rgb;
         rgbcolor_init(&rgb, R, G, B);
 
-        rgbcolor* bestFittingPaletteColor = remap[perfectHashRgbcolor(&rgb)];
+        int colorIndex = 0;
+        labcolor lab;        
+        rgb_to_lab(&rgb, &lab);
+        float minDiff = 100.0f;
 
-        if (!bestFittingPaletteColor) {
-            labcolor lab;        
-            rgb_to_lab(&rgb, &lab);
-            float minDiff = 100.0f;
-            int indexOfBestFittingPaletteColor = 0;
-            for (int i = 0; i < countOfUniquePaletteColors; i++) {
-                float diff = ciede2000(&lab, uniqueLabPaletteColors + i);
-                if (diff < minDiff) {
-                    minDiff = diff;
-                    indexOfBestFittingPaletteColor = i;
-                }
+        for (int i = 0; i < countOfUniquePaletteColors; i++) {
+            float diff = ciede2000(&lab, uniqueLabPaletteColors + i);
+            if (diff < minDiff) {
+                minDiff = diff;
+                colorIndex = i;
             }
-            bestFittingPaletteColor = uniqueRgbPaletteColors + indexOfBestFittingPaletteColor;
-            remap[perfectHashRgbcolor(&rgb)] = bestFittingPaletteColor;
         }
 
-        outputImage[i * inputFormat + 0] = (unsigned char) bestFittingPaletteColor->R;
-        outputImage[i * inputFormat + 1] = (unsigned char) bestFittingPaletteColor->G;
-        outputImage[i * inputFormat + 2] = (unsigned char) bestFittingPaletteColor->B;
-        if (inputFormat == 4) {
-            outputImage[i * inputFormat + 3] = (unsigned char) 255;
-        }
+        image[byte_index] |= (unsigned char)(colorIndex << (byte_half ? 0 : 4));
     }
 
-    free(remap);
+    unsigned char* buffer;
+    size_t buffer_size;
+    if (lodepng_encode(&buffer, &buffer_size, image, inputWidth, inputHeight, &state)) {
+        fprintf(stderr, "Encoder error: %s\n", lodepng_error_text(state.error));
+        free(image);
+        lodepng_state_cleanup(&state);
+        return EXIT_FAILURE;
+    }
 
-    int success = 0;
-	if (strcmp(outputFileExtension, "png") == 0) {
-		success = stbi_write_png(outputFilename, inputWidth, inputHeight, inputFormat, outputImage, 0);
-	} else if (strcmp(outputFileExtension, "bmp") == 0) {
-		success = stbi_write_bmp(outputFilename, inputWidth, inputHeight, inputFormat, outputImage);
-	} else if (strcmp(outputFileExtension, "tga") == 0) {
-		success = stbi_write_tga(outputFilename, inputWidth, inputHeight, inputFormat, outputImage);
-	} else if (strcmp(outputFileExtension, "jpg") == 0) {
-		success = stbi_write_jpg(outputFilename, inputWidth, inputHeight, inputFormat, outputImage, 100);
-	} else {
-		fprintf(stderr, "The file extension \"%s\" is not supported. Please use one of the following instead: png, bmp, tga, jpg\n", outputFileExtension);
-	}
-	if (!success) {
-		fprintf(stderr, "Unable to write %s\n", outputFilename);
-	}
+    if (lodepng_save_file(buffer, buffer_size, outputFilename)) {
+        fprintf(stderr, "Error saving PNG file\n");
+        free(image);
+        free(buffer);
+        lodepng_state_cleanup(&state);
+        return EXIT_FAILURE;
+    }
+    
+    free(image);
+    free(buffer);
+    lodepng_state_cleanup(&state);
 
     stbi_image_free(inputImage);
     stbi_image_free(paletteImage);
-    free(outputImage);
 
     return EXIT_SUCCESS;
 }
